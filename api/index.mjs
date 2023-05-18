3ebbf55a2c738056c136c3dfeedd53e83899e709
YeaGram <managerdims@gmail.com>
2023-05-18 09:22:03 +0800

    upd

--- api/index.mjs
+++ api/index.mjs
@@ -39,6 +39,46 @@ function getStudio($, selector, dataHK) {
     .children(`[data-hk="${replaceDataHk(dataHK, 5, "4")}"]`)
     .text();
 }
+function getRating($, selector, dataHK) {
+  return $(selector)
+    .children(":nth-child(2)")
+    .children(`[data-hk="${replaceDataHk(dataHK, 5, "5")}"]`)
+    .children(
+      `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "5"), 6, "1")}"]`
+    )
+    .children(":nth-child(2)")
+    .text();
+}
+function getFollow($, selector, dataHk) {
+  return $(selector)
+    .children(":nth-child(2)")
+    .children(`[data-hk="${replaceDataHk(dataHk, 5, "5")}"]`)
+    .children(
+      `[data-hk="${replaceDataHk(replaceDataHk(dataHk, 5, "5"), 6, "2")}"]`
+    )
+    .children(":nth-child(2)")
+    .text();
+}
+function getReview($, selector, dataHK) {
+  return $(selector)
+    .children(":nth-child(2)")
+    .children(`[data-hk="${replaceDataHk(dataHK, 5, "5")}"]`)
+    .children(
+      `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "5"), 6, "3")}"]`
+    )
+    .children(":nth-child(2)")
+    .text();
+}
+function getComment($, selector, dataHK) {
+  return $(selector)
+    .children(":nth-child(2)")
+    .children(`[data-hk="${replaceDataHk(dataHK, 5, "5")}"]`)
+    .children(
+      `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "5"), 6, "4")}"]`
+    )
+    .children(":nth-child(2)")
+    .text();
+}
 
 function getComicData(htmlPage) {
   const comic = cheerio.load(htmlPage);
@@ -53,13 +93,23 @@ function getComicData(htmlPage) {
       const comicTitle = getComicTitle(...args);
       const otherTitle = getOtherTitle(...args);
       const studio = getStudio(...args);
+      const rating = getRating(...args);
+      const follow = getFollow(...args);
+      const review = getReview(...args);
+      const comment = getComment(...args);
 
       return {
         title: comicTitle,
         otherTitle: otherTitle.split("/"),
         studio: studio.split("/"),
+        rating: rating,
+        follow: follow,
+        review: review,
+        comment: comment,
         cover: {
           name: cover.title,
+          src: cover.src,
+          altName: cover.alt,
         },
       };
     })
