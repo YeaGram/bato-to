2c478a8217851f9d63e3aba22c0e1249389d8efe
YeaGram <managerdims@gmail.com>
2023-05-18 09:13:20 +0800

    gf asd

--- api/index.mjs
+++ api/index.mjs
@@ -12,123 +12,152 @@ function replaceDataHk(string, idx, val) {
   return splited.join("-");
 }
 
-// function getCover() {}
-
-// function getComicData(htmlPage) {}
-
-class comicScrapper {
-  constructor(htmlPage) {}
-
-  map(htmlPage) {
-    const comic = cheerio.load(htmlPage);
-    const comicContainer = comic("div.border-b-base-200");
-
-    return comicContainer
-      .map((i, el) => {
-        const dataHK = comic(el).attr("data-hk");
-
-        const args = [comic, el, dataHK];
-        const comicTitle = this.getComicTitle(...args);
-        const otherTitle = this.getOtherTitle(...args);
-        const studio = this.getStudio(...args);
-        const rating = this.getRating(...args);
-        const follow = this.getFollow(...args);
-        const review = this.getReview(...args);
-        const comment = this.getComment(...args);
-        const cover = this.getCover(...args);
-
-        return {
-          title: comicTitle,
-          otherTitle: otherTitle.split("/"),
-          studio: studio.split("/"),
-          rating: rating,
-          follow: follow,
-          review: review,
-          comment: comment,
-          cover: {
-            name: cover.title,
-            alt: cover.alt,
-            src: cover.src,
-          },
-        };
-      })
-      .toArray();
-  }
-  getComicTitle($, selector, dataHK) {
-    return $(selector)
-      .children(":nth-child(2)")
-      .children(`[data-hk="${replaceDataHk(dataHK, 5, "2")}"]`)
-      .text();
-  }
-  getOtherTitle($, selector, dataHK) {
-    return $(selector)
-      .children(":nth-child(2)")
-      .children(`[data-hk="${replaceDataHk(dataHK, 5, "3")}"]`)
-      .text();
-  }
-  getStudio($, selector, dataHK) {
-    return $(selector)
-      .children(":nth-child(2)")
-      .children(`[data-hk="${replaceDataHk(dataHK, 5, "4")}"]`)
-      .text();
-  }
-  getRating($, selector, dataHK) {
-    return $(selector)
-      .children(":nth-child(2)")
-      .children(`[data-hk="${replaceDataHk(dataHK, 5, "5")}"]`)
-      .children(
-        `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "5"), 6, "1")}"]`
-      )
-      .children(":nth-child(2)")
-      .text();
-  }
-  getFollow($, selector, dataHk) {
-    return $(selector)
-      .children(":nth-child(2)")
-      .children(`[data-hk="${replaceDataHk(dataHk, 5, "5")}"]`)
-      .children(
-        `[data-hk="${replaceDataHk(replaceDataHk(dataHk, 5, "5"), 6, "2")}"]`
-      )
-      .children(":nth-child(2)")
-      .text();
-  }
-  getReview($, selector, dataHK) {
-    return $(selector)
-      .children(":nth-child(2)")
-      .children(`[data-hk="${replaceDataHk(dataHK, 5, "5")}"]`)
-      .children(
-        `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "5"), 6, "3")}"]`
-      )
-      .children(":nth-child(2)")
-      .text();
-  }
-  getComment($, selector, dataHK) {
-    return $(selector)
-      .children(":nth-child(2)")
-      .children(`[data-hk="${replaceDataHk(dataHK, 5, "5")}"]`)
-      .children(
-        `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "5"), 6, "4")}"]`
-      )
-      .children(":nth-child(2)")
-      .text();
-  }
-  getCover($, selector, dataHK) {
-    return $(selector)
-      .children(`[data-hk="${replaceDataHk(dataHK, 5, "1")}"]`)
-      .children()
-      .children(
-        `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "1"), 6, "1")}"]`
-      )
-      .attr();
-  }
+function getCover($, selector, dataHK) {
+  return $(selector)
+    .children(`[data-hk="${replaceDataHk(dataHK, 5, "1")}"]`)
+    .children()
+    .children(
+      `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "1"), 6, "1")}"]`
+    )
+    .attr();
 }
+
+function getComicData(htmlPage) {
+  const comic = cheerio.load(htmlPage);
+  const comicContainer = comic("div.border-b-base-200");
+
+  return comicContainer
+    .map((i, el) => {
+      const dataHK = comic(el).attr("data-hk");
+      const cover = getCover(comic, el, dataHK);
+      return cover.title;
+    })
+    .toArray();
+}
+
+// class comicScrapper {
+//   constructor(htmlPage) {}
+
+//   map(htmlPage) {
+//     const comic = cheerio.load(htmlPage);
+//     const comicContainer = comic("div.border-b-base-200");
+
+//     return comicContainer
+//       .map((i, el) => {
+//         const dataHK = comic(el).attr("data-hk");
+
+//         const args = [comic, el, dataHK];
+//         const comicTitle = this.getComicTitle(...args);
+//         const otherTitle = this.getOtherTitle(...args);
+//         const studio = this.getStudio(...args);
+//         const rating = this.getRating(...args);
+//         const follow = this.getFollow(...args);
+//         const review = this.getReview(...args);
+//         const comment = this.getComment(...args);
+//         const cover = this.getCover(...args);
+
+//         return {
+//           title: comicTitle,
+//           otherTitle: otherTitle.split("/"),
+//           studio: studio.split("/"),
+//           rating: rating,
+//           follow: follow,
+//           review: review,
+//           comment: comment,
+//           cover: {
+//             name: cover.title,
+//             alt: cover.alt,
+//             src: cover.src,
+//           },
+//         };
+//       })
+//       .toArray();
+//   }
+//   getComicTitle($, selector, dataHK) {
+//     return $(selector)
+//       .children(":nth-child(2)")
+//       .children(`[data-hk="${replaceDataHk(dataHK, 5, "2")}"]`)
+//       .text();
+//   }
+//   getOtherTitle($, selector, dataHK) {
+//     return $(selector)
+//       .children(":nth-child(2)")
+//       .children(`[data-hk="${replaceDataHk(dataHK, 5, "3")}"]`)
+//       .text();
+//   }
+//   getStudio($, selector, dataHK) {
+//     return $(selector)
+//       .children(":nth-child(2)")
+//       .children(`[data-hk="${replaceDataHk(dataHK, 5, "4")}"]`)
+//       .text();
+//   }
+//   getRating($, selector, dataHK) {
+//     return $(selector)
+//       .children(":nth-child(2)")
+//       .children(`[data-hk="${replaceDataHk(dataHK, 5, "5")}"]`)
+//       .children(
+//         `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "5"), 6, "1")}"]`
+//       )
+//       .children(":nth-child(2)")
+//       .text();
+//   }
+//   getFollow($, selector, dataHk) {
+//     return $(selector)
+//       .children(":nth-child(2)")
+//       .children(`[data-hk="${replaceDataHk(dataHk, 5, "5")}"]`)
+//       .children(
+//         `[data-hk="${replaceDataHk(replaceDataHk(dataHk, 5, "5"), 6, "2")}"]`
+//       )
+//       .children(":nth-child(2)")
+//       .text();
+//   }
+//   getReview($, selector, dataHK) {
+//     return $(selector)
+//       .children(":nth-child(2)")
+//       .children(`[data-hk="${replaceDataHk(dataHK, 5, "5")}"]`)
+//       .children(
+//         `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "5"), 6, "3")}"]`
+//       )
+//       .children(":nth-child(2)")
+//       .text();
+//   }
+//   getComment($, selector, dataHK) {
+//     return $(selector)
+//       .children(":nth-child(2)")
+//       .children(`[data-hk="${replaceDataHk(dataHK, 5, "5")}"]`)
+//       .children(
+//         `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "5"), 6, "4")}"]`
+//       )
+//       .children(":nth-child(2)")
+//       .text();
+//   }
+//   getCover($, selector, dataHK) {
+//     return $(selector)
+//       .children(`[data-hk="${replaceDataHk(dataHK, 5, "1")}"]`)
+//       .children()
+//       .children(
+//         `[data-hk="${replaceDataHk(replaceDataHk(dataHK, 5, "1"), 6, "1")}"]`
+//       )
+//       .attr();
+//   }
+// }
+// try {
+//   fetch("https://battwo.com/v3x-search?page=1")
+//     .then((res) => res.text())
+//     .then((data) => {
+//       console.log(new comicScrapper().map(data));
+//     });
+// } catch (error) {
+//   console.error(error);
+// }
 
 app.get("/", (req, res) => {
   try {
     fetch("https://battwo.com/v3x-search?page=1")
       .then((res) => res.text())
       .then((data) => {
-        console.log(new comicScrapper().map(data));
+        res.send(getComicData(data));
+        console.log(getComicData(data));
       });
   } catch (error) {
     console.error(error);
